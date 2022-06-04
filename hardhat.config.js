43287a07ddec17727b4cdfc0f39d61dc5a27906f
RobertLE <robert817le@gmail.com>
2022-06-04 10:35:15 -0500

    dotenv environment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,9 @@
 const { task } = require("hardhat/config");
 
 require("@nomiclabs/hardhat-waffle");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -19,17 +22,13 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-const PRIVATE_KEY =
-  "5a6cda3b8ea1d1b87e12e0166d189f6255bcdd7502a2ae57a0da4bbd0fdda427";
-const INFURA_KEY = "4e8d52d1226e4eda856815ae5b2f486c";
-
 module.exports = {
   solidity: "0.8.4",
   networks: {
     kovan: {
-      url: `https://kovan.infura.io/v3/${INFURA_KEY}`,
+      url: `${process.env.KORVAN_URL}${process.env.INFURA_KEY}`,
       chainId: 42,
-      accounts: [PRIVATE_KEY],
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
